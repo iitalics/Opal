52e6b0086358baa015db45d3f5e6856e355fb48e
Milo Turner <iitalics@gmail.com>
2015-08-26 18:31:08 -1000

    example with lots of foreign and native code

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "env/Loader.h"
 #include "infer/Analysis.h"
 #include "runtime/Cell.h"
@@ -5,17 +6,76 @@
 using namespace Opal;
 
 
+// example custom runtime type
+//  runtime objects:
+//  - must inheret from GC::Object to be garbage collected
+//  - optionally overloads `void markChildren ()`
+struct Foo : public GC::Object
+{
+	static std::vector<std::string> foos;
+	static Env::Type* type;
+
+	Foo () : i(0) {}
+	virtual ~Foo () {}
+
+	// some methods
+	Run::Cell advance ()
+	{
+		std::cout << "===> " << foos[i] << std::endl;
+		i = (i + 1) % foos.size();
+		return Run::Cell::Unit();
+	}
+	Run::Cell str () { return Run::Cell::String(foos[i]); }
+
+	int i;
+};
+std::vector<std::string> Foo::foos {
+	"foo", "bar", "baz"
+};
+Env::Type* Foo::type = nullptr;
+
+
+// constructor
 static void foo (Run::Thread& th)
 {
-	std::cout << "===> foo! <===" << std::endl;
-	th.push(Run::Cell::Unit());
+	auto a = Run::Cell::Object(Foo::type, new Foo());
+	th.push(a);
+	a.release();
 }
+// 'linker functions' for object methods
+static void foo_advance (Run::Thread& th)
+{
+	auto a = th.pop();
+	auto foo = (Foo*) (a.obj);
+	th.push(foo->advance());
+	a.release();
+}
+static void foo_str (Run::Thread& th)
+{
+	auto a = th.pop();
+	auto foo = (Foo*) (a.obj);
+	th.push(foo->str());
+	a.release();
+}
+
+// package loader
 static void loadPackage (Env::Package& pkg)
 {
-	pkg.put("foo", foo);
+	// get type from module
+	auto fooMod = Env::Module::get("Foo");
+	if (!(Foo::type = fooMod->getType("foo")))
+		throw SourceError("no foo type!");
+
+	// export functions
+	pkg
+	.put("foo", foo)
+	.put("foo.advance", foo_advance)
+	.put("foo.str", foo_str);
 
 	std::cout << "load package '" << pkg.name() << "' is a success" << std::endl;
 }
+
+// package request
 static Env::PackageLoad _1("opal.test", loadPackage);
 
 
@@ -27,6 +87,8 @@ int main ()
 	Run::Thread thread;
 	try
 	{
+		// initialize
+		// TODO: group this
 		Infer::Analysis::initTypes();
 		Run::Cell::initTypes();
 
@@ -41,11 +103,11 @@ int main ()
 
 		// execute it in a new thread
 		std::cout << "executing main" << std::endl;
-		thread.call(global_main->func);
-
 		size_t count = 0;
+		thread.call(global_main->func);
 		while (thread.step()) count++;
 
+		// log output
 		std::cout << "total commands: " << count << std::endl;
 		if (thread.size() > 0)
 		{
@@ -55,6 +117,7 @@ int main ()
 		}
 
 		// bye
+		// TODO: destroy everything?
 	}
 	catch (SourceError& err)
 	{
--- tests/external.opal
+++ tests/external.opal
@@ -1,5 +1,7 @@
+module Foo
 use Core
 
+// int types. TODO: declare these seperately
 impl x : int {
 	fn add (y : int) extern("opal.num") "int.add" -> int
 	fn sub (y : int) extern("opal.num") "int.sub" -> int
@@ -9,10 +11,29 @@ impl x : int {
 	fn cmp (y : int) extern("opal.num") "int.sub" -> int
 }
 
-fn foo () extern("opal.test") "foo" -> unit
+
+// external 'foo' type
+// "true" means "garbage collected"
+pub type foo extern true
+impl foo {
+	fn advance ()
+		extern("opal.test") "foo.advance" -> unit
+	fn str ()
+		extern("opal.test") "foo.str" -> string
+}
+fn foo () extern("opal.test") "foo" -> foo
+
+// simple utility
+fn repeat (n : int, f : fn() -> unit) {
+	if n > 0 {
+		f()
+		repeat(n - 1, f)
+	}
+}
 
 fn main () {
-	foo()
+	let my_foo = foo()
 
-	5 * 3 + 4 * 2
+	repeat(5, my_foo.advance)
+	my_foo.str()
 }
