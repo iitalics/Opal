aa716f5a052db05f52095810413c759d21e16d83
Milo Turner <iitalics@gmail.com>
2015-08-29 13:48:51 -1000

    lambda test program (e approximation)

--- src/main.cpp
+++ src/main.cpp
@@ -5,81 +5,6 @@
 #include "runtime/Exec.h"
 using namespace Opal;
 
-
-// example custom runtime type
-//  runtime objects:
-//  - must inheret from GC::Object to be garbage collected
-//  - optionally overloads `void markChildren ()`
-struct Foo : public GC::Object
-{
-	static std::vector<std::string> foos;
-	static Env::Type* type;
-
-	Foo () : i(0) {}
-	virtual ~Foo () {}
-
-	// some methods
-	Run::Cell advance ()
-	{
-		std::cout << "===> " << foos[i] << std::endl;
-		i = (i + 1) % foos.size();
-		return Run::Cell::Unit();
-	}
-	Run::Cell str () { return Run::Cell::String(foos[i]); }
-
-	int i;
-};
-std::vector<std::string> Foo::foos {
-	"foo", "bar", "baz"
-};
-Env::Type* Foo::type = nullptr;
-
-
-// constructor
-static void foo (Run::Thread& th)
-{
-	auto a = Run::Cell::Object(Foo::type, new Foo());
-	th.push(a);
-	a.release();
-}
-// 'linker functions' for object methods
-static void foo_advance (Run::Thread& th)
-{
-	auto a = th.pop();
-	auto foo = (Foo*) (a.obj);
-	th.push(foo->advance());
-	a.release();
-}
-static void foo_str (Run::Thread& th)
-{
-	auto a = th.pop();
-	auto foo = (Foo*) (a.obj);
-	th.push(foo->str());
-	a.release();
-}
-
-// package loader
-static void loadPackage (Env::Package& pkg)
-{
-	// get type from module
-	auto fooMod = Env::Module::get("Test");
-	if (!(Foo::type = fooMod->getType("foo")))
-		throw SourceError("no foo type!");
-
-	// export functions
-	pkg
-	.put("foo", foo)
-	.put("foo.advance", foo_advance)
-	.put("foo.str", foo_str);
-
-	std::cout << "load package '" << pkg.name() << "' is a success" << std::endl;
-}
-
-// package request
-static Env::PackageLoad _1("opal.test", loadPackage);
-
-
-
 int main ()
 {
 	SourceError::color = true;
@@ -93,7 +18,7 @@ int main ()
 		Run::Cell::initTypes();
 
 		// load some code
-		auto nm = Env::loadSource("tests/external.opal");
+		auto nm = Env::loadSource("tests/lambda.opal");
 		Env::finishModuleLoad();
 
 		// find 'main' function
---
+++ tests/lambda.opal
@@ -0,0 +1,47 @@
+use Core
+
+impl x : real {
+	fn add (y : real)   extern("opal.num") "real.add" -> real
+	fn sub (y : real)   extern("opal.num") "real.sub" -> real
+	fn mul (y : real)   extern("opal.num") "real.mul" -> real
+	fn div (y : real)   extern("opal.num") "real.div" -> real
+	fn mod (y : real)   extern("opal.num") "real.mod" -> real
+	fn succ ()          extern("opal.num") "real.succ" -> real
+	fn pred ()          extern("opal.num") "real.pred" -> real
+	fn cmp (y : real)   extern("opal.num") "real.cmp" -> int
+	fn equal (y : real) extern("opal.num") "real.equal" -> bool
+
+	fn to_int () extern("opal.num") "real.to_int" -> int
+	fn to_real () { x }
+	fn to_long () extern("opal.num") "real.to_long" -> long
+}
+
+fn iterate (n : real, z : #a, f : fn(real, #a) -> #a) {
+	if n == 0 {
+		z
+	} else {
+		f(n, iterate(n - 1, z, f))
+	}
+}
+
+type e_approx {
+	sum : real,
+	denom : real
+}
+
+fn main () {
+	let approx = new e_approx { sum = 0, denom = 1 }
+
+	// e = sum[n=0 -> infty] 1 / n!
+	//   = 1 + 1 + 1/2 + 1/6 + 1/24 + ...
+	approx = iterate(100, approx,
+		fn (n, approx) {
+			new e_approx {
+				sum = approx.sum + 1.0 / approx.denom,
+				denom = approx.denom * n
+			}
+		})
+	
+	let E = approx.sum
+	return E
+}
