ee9c5410ac6238bc7527e2df33f46ca35bafc002
Milo Turner <iitalics@gmail.com>
2015-10-12 15:42:10 -1000

    command lines options

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,100 @@
 #include "runtime/Exec.h"
 using namespace Opal;
 
+static std::string prgmName;
+static void usage ()
+{
+	std::cout
+		<< "usage: " << prgmName << " [options] <file> [<files>...]" << std::endl
+		<< "  options:" << std::endl
+		<< "    <file>            opal file to execute, containing main() function" << std::endl
+		<< "    <files>...        additional opal files to execute" << std::endl
+		<< "    -h, --help        show this help text" << std::endl
+		<< "    -m <Module>       require module <Module>" << std::endl
+		<< "    -P <path>         search '<path>/opal_libs' for modules" << std::endl
+		;
+		/*
+		<< "    --debug           enable debug mode" << std::endl
+		<< "    --docs <path>     generate documentation files in <path>" << std::endl
+		*/
+}
+
+
+static bool OptHelp (const std::string& _)
+{ usage(); return true; }
+static bool OptPath (const std::string& path)
+{ Env::searchPaths.insert(path); return false; }
+static bool OptModule (const std::string& name)
+{ Env::loadModule(name); return false; }
+
+static Env::Namespace* loadSources (const std::vector<std::string>& args, size_t& i) {
+	Env::Namespace* nmMain = nullptr;
+
+	for (; i < args.size(); i++)
+	{
+		auto nm = Env::loadSource(args[i]);
+		if (nmMain == nullptr)
+			nmMain = nm;
+	}
+
+	return nmMain;
+}
+
+using OptHandler = bool (*)(const std::string&);
+struct Opt
+{
+	std::string flag;
+	bool takesArg;
+	OptHandler handler;
+};
+
 int main (int argc, char** argv)
 {
 	if (argc < 1) return 2;
 
 	SourceError::color = true;
+
+	std::vector<std::string> args;
+	for (int j = 1; j < argc; j++)
+		args.push_back(std::string(argv[j]));
+
+	std::vector<Opt> opts {
+		{ "-h",     false, OptHelp },
+		{ "--help", false, OptHelp },
+		{ "-P",     true,  OptPath },
+		{ "-m",     true,  OptModule }
+	};
+
+	size_t i = 0;
+	while (i < args.size() && args[i][0] == '-')
+	{
+		auto opt = opts.begin();
+		auto flag = args[i++];
+
+		for (; opt != opts.end(); ++opt)
+			if (opt->flag == flag)
+				break;
+
+		if (opt == opts.end())
+		{
+			std::cout << "invalid option '" << flag << "'" << std::endl;
+			return 1;
+		}
+
+		std::string arg = "";
+		if (opt->takesArg)
+		{
+			if (i >= args.size())
+			{
+				std::cout << "option '" << flag << "' expects an argument" << std::endl;
+				return 1;
+			}
+			arg = args[i++];
+		}
+
+		if (opt->handler(arg))
+			return 0;
+	}
 
 	try
 	{
@@ -17,17 +106,22 @@ int main (int argc, char** argv)
 		Env::initSearchPaths(argv[0]);
 
 		// load some code
-		auto nm = Env::loadSource("tests/in_out.opal");
+		auto nmMain = loadSources(args, i);
+		if (nmMain == nullptr)
+		{
+			usage();
+			return 1;
+		}
 		Env::finishModuleLoad();
 
 		// find 'main' function
-		auto global_main = nm->getGlobal(AST::Name("main"));
-		if (global_main == nullptr || !global_main->isFunc)
+		auto globMain = nmMain->getGlobal(AST::Name("main"));
+		if (globMain == nullptr || !globMain->isFunc)
 			throw SourceError("no main function defined");
 
 		// execute it in a new thread
 		auto thread = Run::Thread::start();
-		thread->call(global_main->func);
+		thread->call(globMain->func);
 //		std::cout << "executing main" << std::endl;
 
 		// run threads until completion
