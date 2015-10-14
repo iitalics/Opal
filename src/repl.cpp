8f4c47b105521c761e3a01f76f9594bbd67840c1
Milo Turner <iitalics@gmail.com>
2015-10-14 00:04:09 -1000

    slight refactoring

--- src/repl.cpp
+++ src/repl.cpp
@@ -10,9 +10,8 @@ static void replEvaluate (const std::string& string,
 		Env::Namespace* nm, Env::Module* module,
 		Run::ThreadPtr thread)
 {
-	// Parse
+	// parse
 	Scanner scan(string, "<input>");
-
 	if (scan.get() == Tokens::END_OF_FILE)
 		return;
 
@@ -20,26 +19,19 @@ static void replEvaluate (const std::string& string,
 	scan.expect(Tokens::END_OF_FILE);
 	Desugar::desugar(ast);
 
-	// Create
+	// create
 	auto fn = new Env::Function(Env::Function::CodeFunction,
 		"<input>", module);
 	fn->nm = nm;
 	fn->body = ast;
 
-	// Infer
+	// analyze
 	fn->infer();
-
-	// CodeGen
 	fn->code = Code::CodeGen::generate(fn);
 
-	// Execute
+	// execute
 	thread->call(fn);
 	while (thread->step()) ;
-
-	// Display
-	auto res = thread->pop();
-	std::cout << res.str() << std::endl;
-	res.release();
 
 	delete fn;
 }
@@ -49,11 +41,13 @@ void runRepl (Run::ThreadPtr thread)
 	std::cout << "@@@  Opal interactive prompt  @@@"
 	          << std::endl << std::endl;
 
+	// create a namespace and private module
 	auto module = new Env::Module();
 	auto nm = new Env::Namespace(module, module);
 
 	while (!std::cin.eof())
 	{
+		// Read
 		std::cout << ">> ";
 		std::cout.flush();
 
@@ -62,12 +56,21 @@ void runRepl (Run::ThreadPtr thread)
 
 		try
 		{
+			// Eval
 			replEvaluate(input, nm, module, thread);
 		}
 		catch (std::exception& err)
 		{
 			std::cerr << err.what() << std::endl;
+			continue;
 		}
+
+		// Print
+		auto res = thread->pop();
+		std::cout << res.str() << std::endl;
+		res.release();
+
+		// Loop
 	}
 	std::cout << std::endl;
 }
